2f2b7a9501485db33467d5bee3a26bef18d0bd20
Yasir Yilmazcoban <34142336+yasirylmzcbn@users.noreply.github.com>
2026-02-10 09:51:39 -0600

    add vr origin as player

---
+++ Assets/Scripts/IInteractable.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookverseVR.Assets.Scripts
+{
+    public interface IInteractable
+    {
+        public bool Interact();
+    }
+}
